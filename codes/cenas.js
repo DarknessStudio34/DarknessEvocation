7dfb28623966f663b4554e2397464d87bf0a5ea9
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2022-12-28 15:11:37 -0300

    Update cenas.js

--- codes/cenas.js
+++ codes/cenas.js
@@ -16,7 +16,8 @@ function cenarios (){
       }
       if (apm == 1){
         console.log('funcionando')
-        Xpm = Xpm - 20
+        Xpm = Xpm - 50
+
         image(BGs.menu1_1, Xpm, Ypm, window.screen.width, screenY)
       }
       image (BGs.menu_options1, 0, 0, screenX, screenY );
