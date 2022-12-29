87d3cc69b612a9d2f62fa0e606480d85d9a413d1
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2022-12-28 21:45:30 -0300

    Update vars.js

--- codes/vars.js
+++ codes/vars.js
@@ -1,13 +1,13 @@
 var c = 0;
 var lg
 var timer = 0
-var BGs
-var mbp = false
-var bop
-var Xpm = 0
-var Ypm = 0
-var apm = 0
-
+var BGs;
+var mbp = false;
+var bop;
+var Xpm = 0;
+var Ypm = 0;
+var apm = 0;
+var xbm = 0;
 var screenX = window.screen.width
 var screenY = window.screen.height
 var propor
