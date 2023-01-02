7f4bd998d2ce14379281bb68220fb4bbad31c002
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-01-02 13:00:11 -0300

    Update engine.js

--- codes/engine.js
+++ codes/engine.js
@@ -20,7 +20,7 @@ function draw() {
 function preload (){
   lg = loadImage('assets/logo_DS.png')
   BGs = {
-    menu2: loadImage('assets/menus/menu_principal/menu_bg_2.png'),
+    menu2: loadImage('assets/menus/menu_principal/menu_bg_2.png.png'),
     menu1: loadImage('assets/menus/menu_principal/menu_bg_1.png'),
     menu1_1: loadImage('assets/menus/menu_principal/menu_bg_11.png'),
     menu_play1: loadImage('assets/menus/menu_principal/play1.png'),
